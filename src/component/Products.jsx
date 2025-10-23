ba08805e229001d8b10cbdc2d49a20e79dbb9cfe
ayazcoder <azkhan03139@gmail.com>
2025-10-23 15:58:41 +0500

    change comitt

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
         <h1 className='text-3xl font-bold text-center pt-10'>Products</h1>
         {/* comming soon */}
         <p className='my-24 text-4xl text-center font-semibold'>
-            Comming Soon...
+            Comming Soon In November...
         </p>
     </div>
   )
